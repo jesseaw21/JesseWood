d6f3a6a80808d192f752124bb5432a5d8b100cd8
jesseaw21 <jessewood21@gmail.com>
2015-09-03 12:57:12 -0700

    Updates to UI

---
+++ JesseWood/JesseWood/Scripts/jwood.js
@@ -0,0 +1,25 @@
+﻿var didScroll = false;
+var scrollHeadCreated = false;
+
+$(window).scroll(function () {
+    didScroll = true;
+});
+
+setInterval(function () {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 500);
+
+function hasScrolled() {
+    if ($(this).scrollTop() > 80 && scrollHeadCreated == false) {
+        var scrollHead = document.createElement("header");
+        scrollHead.id = "bunko";
+        document.body.appendChild(scrollHead);
+        scrollHeadCreated = true;
+    } else if ($(this).scrollTop() < 90 && scrollHeadCreated == true) {
+        $("#bunko").remove();
+        scrollHeadCreated = false;
+    }
+}
